0e5ec8ee90638258823eacf9a1ea362b2a16b9cf
wakatabe <wakataberyo@gmail.com>
2015-08-26 20:08:31 +0900

    shift impletmented

--- utils/minutely_sh/run.sh
+++ utils/minutely_sh/run.sh
@@ -3,16 +3,17 @@
 . list225
 . listOther
 list=(${list225[*]} ${listOther[*]})
+date_shift="1 day ago"
 
 for i in "${list[@]}"
 do
     mkdir $i 2> /dev/null
-    wget -q `date +"http://k-db.com/stocks/$i-T/minutely?date=%Y-%m-%d&download=csv" --date "1 day ago"` -Otmp
-    grep `date +"%Y-%m-%d" --date "1 day ago"` tmp
+    wget -q `date +"http://k-db.com/stocks/$i-T/minutely?date=%Y-%m-%d&download=csv"  --date "$date_shift"` -Otmp
+    grep `date +"%Y-%m-%d" --date "$date_shift"` tmp
     if [ $? -eq 1 ]
     then
         continue
     fi
-    sed -e "1,2d" tmp > $i/`date +"%Y%m%d" --date "1 day ago"`
+    sed -e "1,2d" tmp > $i/`date +"%Y%m%d" --date "$date_shift"`
     sleep 1
 done
