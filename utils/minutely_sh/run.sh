8b785470e77b85ef32064a264d765530d0483d2b
hamko <wakataberyo@gmail.com>
2015-08-26 19:41:15 +0900

    makaizou

--- utils/minutely_sh/run.sh
+++ utils/minutely_sh/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 . list225
 . listOther
 list=(${list225[*]} ${listOther[*]})
@@ -6,12 +7,12 @@ list=(${list225[*]} ${listOther[*]})
 for i in "${list[@]}"
 do
     mkdir $i 2> /dev/null
-    wget -q `date +"http://k-db.com/stocks/$i-T/minutely?date=%Y-%m-%d&download=csv"` -Otmp
-    grep `date +"%Y-%m-%d"` tmp
+    wget -q `date +"http://k-db.com/stocks/$i-T/minutely?date=%Y-%m-%d&download=csv" --date "1 day ago"` -Otmp
+    grep `date +"%Y-%m-%d" --date "1 day ago"` tmp
     if [ $? -eq 1 ]
     then
         continue
     fi
-    sed -e "1,2d" tmp > $i/`date +"%Y%m%d"`
+    sed -e "1,2d" tmp > $i/`date +"%Y%m%d" --date "1 day ago"`
     sleep 1
 done
