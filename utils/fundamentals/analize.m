115ac2099ede760d8ce7179e8a34b372d3fd86f0
hamko <wakataberyo@gmail.com>
2015-11-11 12:35:06 +0900

    added names

--- utils/fundamentals/analize.m
+++ utils/fundamentals/analize.m
@@ -17,28 +17,33 @@ M_pbr = M(M(:, 8) != 0, :);
 save M_per.dat M_per
 save M_pbr.dat M_pbr
 
-# 銘柄，PER妥当株価，PBR妥当株価，アナリスト人数，レーティング, PER妥当株価と現在との差の割合，PBR妥当株価と現在との差の割合
+# 銘柄，PER妥当株価のσ，PBR妥当株価のσ，アナリスト人数，レーティング, PER妥当株価と現在との差の割合，PBR妥当株価と現在との差の割合
 selection = [1, 5, 8, 9, 11, 17, 19]; 
 
 #全てソートのキーは，対PER妥当株価騰落率で行なっている．
 
 # 割安注目株(アナリスト5人以上, レーティング4.0以上, PBR, PERともに標準偏差-1.5以下)なもの
 M_Economical = M(M(:, 5) < -1.5 & M(:, 8) < -1.5 & M(:, 9) > 4 & M(:, 11) > 4.0, selection);
-M_Economical = sortrows(M_Economical, 6)
+M_Economical = sortrows(M_Economical, 6);
 
 # 割高注目株(アナリスト5人以上, PBR, PERともに標準偏差-1.5以下)なもの
 M_Expensive = M(M(:, 5) > +1.5 & M(:, 8) > +1.5 & M(:, 9) > 4, selection);
-M_Expensive = sortrows(M_Expensive, 6)
+M_Expensive = flipud(sortrows(M_Expensive, 6));
 
 M_GoodCompany = M(M(:, 11)>4.0 & M(:, 9) > 4, selection);
-M_GoodCompany = sortrows(M_GoodCompany, 6)
+M_GoodCompany = sortrows(M_GoodCompany, 6);
+
+M_Market = [mean(M(M(:, 5) != 0, 5)) median(M(M(:, 5) != 0, 5))];
+M_GoodMarket = [mean(M_GoodCompany(:, 2)) median(M_GoodCompany(:, 2))];
  
 save M_Economical.dat M_Economical
 save M_Expensive.dat M_Expensive
 save M_GoodCompany.dat M_GoodCompany
+save M_Market.dat M_Market
+save M_GoodMarket.dat M_GoodMarket
 
 
 #hist(M(M(:,17)!=0, 5), 20); figure;       # PER妥当株価の差のσ
 #hist(M(M(:,17)!=0, 17), 20); figure;      # PER妥当株価との差の割合
-figure; hist(M_GoodCompany(:, 2), 20);     # 注目株のPER妥当株価の差のσ
-figure; hist(M_GoodCompany(:, 6), 20);     # 注目株のPER妥当株価との差の割合
+#figure; hist(M_GoodCompany(:, 2), 20);     # 注目株のPER妥当株価の差のσ
+#figure; hist(M_GoodCompany(:, 6), 20);     # 注目株のPER妥当株価との差の割合
--- utils/fundamentals/mail.sh
+++ utils/fundamentals/mail.sh
@@ -3,6 +3,26 @@
 cp `date +"%Y%m%d" --date "1 day ago"` a
 octave analize.m
 
-cat M_Expensive.dat | mail -s `date +"%Y%m%d" --date "1 day ago"` wakataberyo@gmail.com
-cat M_Economical.dat | mail -s `date +"%Y%m%d" --date "1 day ago"` wakataberyo@gmail.com
-cat M_GoodCompany.dat | mail -s `date +"%Y%m%d" --date "1 day ago"` wakataberyo@gmail.com
+echo "おはようございます。スイング担当の待鳥すいです。" >> tmp
+echo `date +"%Y年%d月%m日のファンデメンタル情報をお伝えします。" --date "1 day ago"` >> tmp
+
+echo >> tmp
+echo "市場の過熱感の報告です。" >> tmp
+awk '$0!~"#"&&$0~" "{printf("市場全体は、平均%.2fσ、中央値%.2fσの状態にあります。\n", $1, $2);}' M_Market.dat >> tmp
+awk '$0!~"#"&&$0~" "{printf("レーティングの高い会社に限定すると、平均%.2fσ、中央値%.2fσの状態にあります。\n", $1, $2);}' M_GoodMarket.dat >> tmp
+
+echo >> tmp
+echo "割安株の報告です。" >> tmp
+awk '$0!~"#"&&$0~" "{printf("銘柄番号%dが%.1fσで割安です。妥当株価の%.1f%です。（%.1f by %d）\nhttp://kabuyoho.ifis.co.jp/index.php?action=tp1&sa=report_per&bcode=%d\n", $1, $2, $6*100, $5, $4, $1);}' M_Economical.dat >> tmp
+
+echo >> tmp
+echo "割高株の報告です。" >> tmp
+awk '$0!~"#"&&$0~" "{printf("銘柄番号%dが%.1fσで割高です。妥当株価の%.1f%です。（%.1f by %d）\nhttp://kabuyoho.ifis.co.jp/index.php?action=tp1&sa=report_per&bcode=%d\n", $1, $2, $6*100, $5, $4, $1);}' M_Expensive.dat >> tmp
+
+echo >> tmp
+echo "以上です。今日も一日頑張ってください。" >> tmp
+
+cat tmp | mail -s `date +"%Y/%m/%d" --date "1 day ago"` wakataberyo@gmail.com 
+cp tmp `date +"%Y%m%d" --date "1 day ago"`.report 
+
+rm tmp
