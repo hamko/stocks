ca777c6ac7d5f9513227bbad53d9755579ee2490
hamko <!!!your>
2020-03-27 01:55:55 +0900

    高速化した結果、過去にめっちゃ投資すればいいよねみたいな自明な結果を出すようになった。インフレ率と、投資額の分布をなるべく均等にするみたいな処理が必要

--- sp500/monthly-stock-analysis.cpp
+++ sp500/monthly-stock-analysis.cpp
@@ -44,10 +44,18 @@ size_t random_seed; struct init_{init_(){ ios::sync_with_stdio(false); cin.tie(0
 #define INF (ll)1e18
 #define mo  (ll)(1e9+7)
 
+using param_t = vector<double>;
+
 // 平均x円・最低y円を毎月積み立てるドルコスト平均法でS&P 500にインデックス投資
 // 短中長期で割安度を偏差値で計算して、線形結合で投資額に反映
 // 最適化前後で46%資産が変わる（リークしまくりなので3割がけだとしてもやらない理由はない）
+vector<double> sp500_history;
+vector<string> sp500_dates;
+ll minYen = 33333, meanYen = 200000;
+ll cap = meanYen * 5; // yen/sigma: 1 sigmaで100万円投入が最大可能性
+bool verbose_mode = 0;
 
+// aの中のxの標準偏差を出力する
 double getSigma(vector<double>& a, double x) {
     if (a.size() <= 1) return 0;
     double m = accumulate(all(a), 0.0) / a.size(), var = 0;
@@ -55,11 +63,11 @@ double getSigma(vector<double>& a, double x) {
     x -= m;
     return x / sqrt(var);
 }
+// return: ret[i] = a(i-len_max, i]の月の株価から見て、i番目の月が標準偏差どれくらいの株価か
 vector<double> getSigmaArray(vector<double>& a, ll len_max) {
     vector<double> ret;
-    ll len = 0;
     rep(i, a.size()) {
-        len = min(i + 1, len_max);
+        ll len = min(i + 1, len_max);
         vector<double> dataset; repi(j, max(0ll, i-len+1), i+1) dataset.pb(a[j]);
         double sigma = getSigma(dataset, a[i]);
         ret.pb(sigma);
@@ -67,72 +75,115 @@ vector<double> getSigmaArray(vector<double>& a, ll len_max) {
     return ret;
 }
 
-// param[0]: min investment, param[1~]: len_max_range
+// return: ret[0]が-1ならエラー
+// return: ret[0]が-1でないなら、ret[0] = 解散利益円, ret[1] = 総投資円
+//
+// param[0]: 月あたりの最小投資円 (積立額)
+// param[1]: 月あたりの目標投資円 (貯金額*ポートフォリオの株式相当額)
+// param[2:2+len_max_range.size()): 短期から長期時間軸で見るときのウィンドウ幅のリスト
 const vll len_max_range = {2, 3, 4, 6, 12, 24, 48};
-ll minYen = 33333, meanYen = 200000;
-tuple<double, vector<double>> evaluate(vector<double>& a, vector<double>& params) {
-   ll n = a.size();
-   vector<double> invest_amount_yen(n, params[0]);
+tuple<double, vector<double>> evaluate(param_t& params) {
+   vector<double> invest_amount_yen(sp500_history.size(), params[0]);
    rep(j, len_max_range.size()) {
        auto len_max = len_max_range[j];
-       auto sigma_array = getSigmaArray(a, len_max);
-       assert(sigma_array.size() == n);
-       rep(i, n) if (sigma_array[i] < 0)
+       auto sigma_array = getSigmaArray(sp500_history, len_max);
+       rep(i, sp500_history.size()) if (sigma_array[i] < 0)
            invest_amount_yen[i] += -sigma_array[i] * params[j+2];
    }
-   if (accumulate(all(invest_amount_yen), 0.0) / invest_amount_yen.size() > params[1]) {
+   rep(i, sp500_history.size()) {
+       chmin(invest_amount_yen[i], cap);
+   }
+   if (accumulate(all(invest_amount_yen), 0.0) / invest_amount_yen.size() > params[1]) { // 投資平均額が大きすぎる
        return mt(-1, vector<double>());
    } else {
-       double stock_num = 0; rep(i, n) stock_num += invest_amount_yen[i] / a[i];
-       return mt(stock_num * a.back() - accumulate(all(invest_amount_yen), 0.0), invest_amount_yen);
+       double stock_num = 0; rep(i, sp500_history.size()) stock_num += invest_amount_yen[i] / sp500_history[i]; // 総取得株数計算
+       return mt(stock_num * sp500_history.back() - accumulate(all(invest_amount_yen), 0.0), invest_amount_yen); // 現在価格での解散利益を計算
    }
 }
 
-ll cap = 1000000;
-vector<double> generateParams(void) {
+param_t generateParams(void) {
     vector<double> ret(2+len_max_range.size());
     ret[0] = minYen, ret[1] = meanYen;
     repi(i, 2, 2+len_max_range.size())
         ret[i] = rand() % cap;
     return ret;
 }
-vector<double> baseline(void) {
-    vector<double> ret(2+len_max_range.size());
+param_t baseline(void) { // 単純なDCA
+    param_t ret(2+len_max_range.size());
     ret[0] = minYen, ret[1] = meanYen;
     return ret;
 }
 
-void randomSearch(vector<double>& a) {
-    double best = -INF; vector<double> best_params;
+
+void initQ(vector<tuple<double, param_t, vector<double>>>& q) {
+    vector<vector<double>> cands= {
+        {33333, 200000, 1.2e6, 3e6},
+    };
+    for (param_t params : cands) {
+        auto tmp = evaluate(params); double profit = get<0>(tmp); vector<double> investment = get<1>(tmp);
+        if (profit < 0) continue; // 投資平均額が大きすぎるなど異常状態
+        q.pb(mt(profit, params, investment));
+    }
+}
+
+void randomSearch(vector<string> dates) {
+    param_t baseline_params = baseline();
+    auto baseline_result = get<0>(evaluate(baseline_params));
+    vector<tuple<double, param_t, vector<double>>> q, q_next;
+    ll NUM_ELETE = 4;
     while (1) {
-        auto params = generateParams(); auto tmp = evaluate(a, params);
-        double v = get<0>(tmp); vector<double> invest_amount_yen = get<1>(tmp);
-        if (v < 0) continue;
-        if (best < v) {
-            best = v; best_params = params;
-            cout << "################" << endl;
-            rep(i, a.size()) cout << a[i] << " " << invest_amount_yen[i] << endl;
-            cout << best << " yen [earning]" << endl;
-            cout << params << " params" << endl;
-            auto baseline_params = baseline();
-            cout << get<0>(evaluate(a, baseline_params)) << endl;
+        q_next = q;
+        // Random parameters
+        rep(_, 4) {
+            auto params = generateParams();
+            auto tmp = evaluate(params); double profit = get<0>(tmp); vector<double> investment = get<1>(tmp);
+            if (profit < 0) continue; // 投資平均額が大きすぎるなど異常状態
+            q_next.pb(mt(profit, params, investment));
         }
+        for (auto x : q) {
+            auto params = get<1>(x);
+            rep(_, 4) {
+                auto next_params = params;
+                repi(i, 2, 2+len_max_range.size()) {
+                    next_params[i] += cap / 100 - 2ll * (rand() % (cap / 100));
+                    chmax(next_params[i], 0);
+                }
+                auto tmp = evaluate(next_params); double profit = get<0>(tmp); vector<double> investment = get<1>(tmp);
+                q_next.pb(mt(profit, next_params, investment));
+            }
+        }
+        sort(all(q_next)); reverse(all(q_next));
+
+        q.clear();
+        rep(_, min<ll>(q_next.size(), NUM_ELETE)) {
+            q.pb(q_next[_]);
+        }
+        if (!q.size()) continue;
+        auto best_profit = get<0>(q[0]); auto best_params = get<1>(q[0]); auto best_investment = get<2>(q[0]);
+        cout << "################" << endl;
+        if (verbose_mode) {
+            for (auto x : q) cout << get<0>(x) << " " << get<1>(x) << endl;
+            cout << "--------" << endl;
+            for (auto x : q_next) cout << get<0>(x) << " " << get<1>(x) << endl;
+
+            rep(i, sp500_history.size()) cout << dates[i] << " " << sp500_history[i] << " " << best_investment[i] << endl;
+        }
+        cout << best_investment.back() << " yen [now]" << endl;
+        cout << best_profit << " yen [earning]" << endl;
+        cout << best_params << " params" << endl;
+        cout << baseline_result * meanYen / minYen<< endl;
     }
 }
 
 int main(int argc, char** argv) {
-    double check_num = -1;
+    double current_sp500 = -1;
     if (argc >= 2)
-        check_num = atof(argv[1]);
+        current_sp500 = atof(argv[1]);
     if (argc >= 3)
-        minYen = atoll(argv[2]);
-    if (argc >= 4)
-        meanYen = atoll(argv[3]);
+        verbose_mode = 1;
 
-    string dummy;
-    vector<double> a; double x; while (cin >> dummy >> dummy >> dummy >> x) a.pb(x); // monthly sp500
-    cout << a << endl;
-    if (check_num >= 0) a.pb(check_num);
-    randomSearch(a);
+    string date; double x; while (cin >> date >> x) sp500_dates.pb(date), sp500_history.pb(x);
+    if (current_sp500 >= 0) sp500_dates.pb("current"), sp500_history.pb(current_sp500);
+    randomSearch(sp500_dates);
     return 0;
 }
